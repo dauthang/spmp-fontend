e172c5f2632d75240d472124bfa7d7f7740087c6
Darius Dau <thangdt@vmodev.com>
2023-07-06 14:47:59 +0700

    update bacground and card classic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Delivery from "@/components/delivery/delivery";
 import ImageBackground from "@/components/image-background/image-background";
 import BackgroundLoading from "@/components/background-loading/background-loading";
 import { useEffect, useState } from "react";
+import BackgroundClassic from "@/components/background-classic/background-classic";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -31,7 +32,8 @@ export default function RootLayout({
             <Header />
             <div className="flex flex-col w-full h-full min-h-screen flex-grow">
               <div className="pt-[90px] px-3 pb-[50px] flex-auto md:px-[35px]">
-                <Delivery />
+                {/* <Delivery /> */}
+                <BackgroundClassic />
                 <Order />
                 <div>{children}</div>
                 <ImageBackground />
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,10 +1,12 @@
+import ListCardClassic from "@/components/list-card-classic/card-classic";
 import ListCard from "@/components/list-card/list-card";
 import Layout from "../layout";
 
 const Product = () => {
   return (
     <div>
-      <ListCard></ListCard>
+      {/* <ListCard></ListCard> */}
+      <ListCardClassic></ListCardClassic>
     </div>
   );
 };
